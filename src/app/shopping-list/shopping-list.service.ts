8054cd31221daf889a35b9aa50625fa8e36721c7
rob212 <robertmcbryde83@gmail.com>
2019-01-06 19:03:26 +0000

    Implemented the shopping list service to allow the shopping list component to get all the current ingredients and to add a new ingredient which emits an event to subscribers passing a new copy of the updated ingredients.

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
 
-  constructor() { }
+  private ingredients = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Pears', 3)
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.getIngredients());
+  }
 }
